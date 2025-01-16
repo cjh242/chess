e9b4e2ca307aaacb80512009bfd0e82edf48f36d
Conway Hogan <conway2019hogan@gmail.com>
2025-01-15 19:22:49 -0700

    rook done

--- shared/src/main/java/chess/MoveCalculators/RookMovesCalculator.java
+++ shared/src/main/java/chess/MoveCalculators/RookMovesCalculator.java
@@ -10,6 +10,50 @@ import java.util.Collection;
 
 public class RookMovesCalculator {
     public static Collection<ChessMove> CalculateRookMoves(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor color) {
-        return new ArrayList<>();
+        // King can go on space in any direction
+        Collection<ChessMove> moves = new ArrayList<>();
+
+        Collection<ChessPosition> positions = new ArrayList<>();
+        ChessPosition position;
+
+        //go down the row
+        for(int i = myPosition.getRow() + 1; i <= 8; i++){
+            position = new ChessPosition(i, myPosition.getColumn());
+            positions.add(position);
+            if(board.getPiece(position) != null) {
+                break;
+            }
+        }
+        for(int i = myPosition.getRow() - 1; i >= 1; i--){
+            position = new ChessPosition(i, myPosition.getColumn());
+            positions.add(position);
+            if(board.getPiece(position) != null) {
+                break;
+            }
+        }
+
+        //go down the columm
+        for(int i = myPosition.getColumn() + 1; i <= 8; i++){
+            position = new ChessPosition(myPosition.getRow(), i);
+            positions.add(position);
+            if(board.getPiece(position) != null) {
+                break;
+            }
+        }
+        for(int i = myPosition.getColumn() - 1; i >= 1; i--){
+            position = new ChessPosition(myPosition.getRow(), i);
+            positions.add(position);
+            if(board.getPiece(position) != null) {
+                break;
+            }
+        }
+
+        for(ChessPosition p : positions){
+            if(board.isValidPosition(p, color)){
+                moves.add(new ChessMove(myPosition, p, null));
+            }
+        }
+
+        return moves;
     }
 }
