76a11a01bc78fe5ea44c17dffca3c67c9089b360
Conway Hogan <conway2019hogan@gmail.com>
2025-02-25 14:47:36 -0700

    changes to main

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,12 +1,20 @@
 import chess.*;
+import dataaccess.IGameDataDAO;
+import dataaccess.MemoryGameDataDAO;
 import server.Server;
+import service.GameService;
+import service.UserService;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
 
-        var server = new Server();
+        IGameDataDAO gameData = new MemoryGameDataDAO();
+        var gameService = new GameService(gameData);
+        var userService = new UserService();
+
+        var server = new Server(gameService, userService);
         server.run(8080);
     }
 }
