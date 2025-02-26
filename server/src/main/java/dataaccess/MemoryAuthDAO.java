ad35a6876158c52333ce103b6c14b177c938c6df
Conway Hogan <conway2019hogan@gmail.com>
2025-02-25 19:51:35 -0700

    adding uuid for authtoken

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,4 +1,26 @@
 package dataaccess;
 
+import dataobjects.AuthData;
+import exception.ResponseException;
+
+import java.util.HashMap;
+import java.util.UUID;
+
 public class MemoryAuthDAO implements IAuthDAO {
+    final private HashMap<String, AuthData> auths = new HashMap();
+
+    public AuthData addAuth(AuthData auth) throws ResponseException {
+        auth = new AuthData(generateToken(), auth.username());
+
+        auths.put(auth.username(), auth);
+        return auth;
+    }
+
+    public AuthData getAuthByUsername(String username) throws ResponseException {
+        return auths.get(username);
+    }
+
+    private static String generateToken() {
+        return UUID.randomUUID().toString();
+    }
 }
