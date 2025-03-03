41945e5f53b420dce77b4eebb53406070247d748
Conway Hogan <conway2019hogan@gmail.com>
2025-03-02 19:49:30 -0700

    rewriting the tests

--- server/src/test/java/service/GameServiceTests.java
+++ server/src/test/java/service/GameServiceTests.java
@@ -23,75 +23,137 @@ public class GameServiceTests {
     static final AuthService authService = new AuthService( new MemoryAuthDAO());
     static final GameService gameService = new GameService(gameDao, authService);
 
-//    @BeforeEach
-//    void clear() throws ResponseException {
-//        gameService.deleteAllGames();
-//        authService.deleteAllAuths();
-//    }
+    @BeforeEach
+    void clear() throws DataAccessException {
+        gameService.deleteAllGames();
+        authService.deleteAllAuths();
+    }
 
-//    @Test
-//    @DisplayName("Add Game Valid Test")
-//    public void addGame() throws ResponseException {
-//        var auth = addAuthForTests();
-//        var gameRequest = new CreateGameRequest(auth.authToken(), "");
-//        var game = gameService.addGame(gameRequest);
-//
-//        var games = gameService.listGames(auth.authToken());
-//        assertEquals(1, games.size());
-//        assertTrue(games.contains(game));
-//    }
+    @Test
+    @DisplayName("Add Game Valid Test")
+    public void addGame() throws DataAccessException {
+        var auth = addAuthForTests();
+        var gameRequest = new CreateGameRequest(auth.authToken(), "");
+        var game = gameService.addGame(gameRequest);
 
-//    @Test
-//    @DisplayName("List Games Valid Test")
-//    public void listGames() throws ResponseException {
-//        var auth = addAuthForTests();
-//        List<GameData> expected = new ArrayList<>();
-//        expected.add(gameService.addGame(new CreateGameRequest(auth.authToken(), "")));
-//        expected.add(gameService.addGame(new CreateGameRequest(auth.authToken(), "")));
-//        expected.add(gameService.addGame(new CreateGameRequest(auth.authToken(), "")));
-//
-//        var actual = gameService.listGames(auth.authToken());
-//        assertIterableEquals(expected, actual);
-//    }
+        var games = gameService.listGames(auth.authToken());
+        assertEquals(1, games.games().size());
+        assertTrue(games.games().stream().anyMatch(x -> x.gameID() == game.gameID()));
+    }
 
-//    @Test
-//    @DisplayName("Delete All Games Valid Test")
-//    public void deleteAllGames() throws ResponseException {
-//        var auth = addAuthForTests();
-//        gameService.addGame(new CreateGameRequest(auth.authToken(), ""));
-//        gameService.addGame(new CreateGameRequest(auth.authToken(), ""));
-//        gameService.addGame(new CreateGameRequest(auth.authToken(), ""));
-//
-//        gameService.deleteAllGames();
-//
-//        var games = gameService.listGames(auth.authToken());
-//        assertEquals(0, games.size());
-//    }
+    @Test
+    @DisplayName("Add Game Unauthorized")
+    public void unauthorizedAddGame(){
+        var result = gameService.addGame(new CreateGameRequest("BAD TOKEN", ""));
 
-//    @Test
-//    @DisplayName("Join Game Valid Test")
-//    public void joinGame() throws ResponseException {
-//        var auth = addAuthForTests();
-//        var user = addUserForTests();
-//        var game = new GameData(1, null, null, "test", new ChessGame());
-//        //calling update here will actually add the game, and skip normal adding protocols
-//        gameDao.update(game);
-//        var joinReq = new JoinGameRequest(ChessGame.TeamColor.BLACK, game.gameID(), auth.authToken());
-//        gameService.joinGame(joinReq);
-//        game = game.addBlackUsername(user.username());
-//
-//        var games = gameService.listGames(auth.authToken());
-//        assertEquals(1, games.size());
-//        assertTrue(games.contains(game));
-//    }
+        assertEquals(401, result.code());
+    }
 
-//    private AuthData addAuthForTests() throws ResponseException {
-//        return authService.addAuth("testUser");
-//    }
-//
-//    private UserData addUserForTests() throws ResponseException {
-//        return userDao.addUser(new RegisterRequest("testUser", "password", "test@email.com"));
-//    }
+    @Test
+    @DisplayName("List Games Valid Test")
+    public void listGames() throws DataAccessException {
+        var auth = addAuthForTests();
+
+        var game1 = gameService.addGame(new CreateGameRequest(auth.authToken(), ""));
+        var game2 = gameService.addGame(new CreateGameRequest(auth.authToken(), ""));
+        var game3 = gameService.addGame(new CreateGameRequest(auth.authToken(), ""));
+
+        List<GameData> expected = new ArrayList<>();
+        expected.add(new GameData(game1.gameID(), null, null, ""));
+        expected.add(new GameData(game2.gameID(), null, null, ""));
+        expected.add(new GameData(game3.gameID(), null, null, ""));
+
+        var actual = gameService.listGames(auth.authToken()).games();
+        assertIterableEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("List Games Unauthorized Test")
+    public void listGamesUnauthorized() {
+        var result = gameService.listGames("BAD TOKEN");
+
+        assertEquals(401, result.code());
+    }
+
+    @Test
+    @DisplayName("Delete All Games Valid Test")
+    public void deleteAllGames() throws DataAccessException {
+        var auth = addAuthForTests();
+        gameService.addGame(new CreateGameRequest(auth.authToken(), ""));
+        gameService.addGame(new CreateGameRequest(auth.authToken(), ""));
+        gameService.addGame(new CreateGameRequest(auth.authToken(), ""));
+
+        gameService.deleteAllGames();
+
+        var games = gameService.listGames(auth.authToken());
+        assertEquals(0, games.games().size());
+    }
+
+    @Test
+    @DisplayName("Join Game Valid Test")
+    public void joinGame() throws DataAccessException {
+        var auth = addAuthForTests();
+        var user = addUserForTests();
+        var game = addGameForTests();
+        var joinReq = new JoinGameRequest(ChessGame.TeamColor.BLACK, game.gameID(), auth.authToken());
+        var result = gameService.joinGame(joinReq);
+        game = game.addBlackUsername(user.username());
+
+        var gamesResult = gameService.listGames(auth.authToken());
+        var games = new ArrayList<>(gamesResult.games());
+
+        assertEquals(1, games.size());
+        assertEquals(game.gameID(), games.getFirst().gameID());
+        assertEquals(200, result.code());
+    }
+
+    @Test
+    @DisplayName("Join Game Unauthorized Test")
+    public void unAuthorizedJoinGame() throws DataAccessException {
+        var game = addGameForTests();
+        var joinReq = new JoinGameRequest(ChessGame.TeamColor.BLACK, game.gameID(), "BAD TOKEN");
+        var result = gameService.joinGame(joinReq);
+
+        assertEquals(401, result.code());
+    }
+
+    @Test
+    @DisplayName("Join Game Bad ID Test")
+    public void badIDJoinGame() throws DataAccessException {
+        var auth = addAuthForTests();
+        var joinReq = new JoinGameRequest(ChessGame.TeamColor.BLACK, 999, auth.authToken());
+        var result = gameService.joinGame(joinReq);
+
+        assertEquals(400, result.code());
+    }
+
+    @Test
+    @DisplayName("Join Game Already Taken Test")
+    public void alreadyTakenJoinGame() throws DataAccessException {
+        var auth = addAuthForTests();
+        var user = addUserForTests();
+        var game = addGameForTests();
+        var joinReq = new JoinGameRequest(ChessGame.TeamColor.BLACK, game.gameID(), auth.authToken());
+        gameService.joinGame(joinReq);
+
+        var result = gameService.joinGame(joinReq);
+
+        assertEquals(403, result.code());
+    }
+
+    private AuthData addAuthForTests() throws DataAccessException {
+        return authService.addAuth("testUser");
+    }
+
+    private UserData addUserForTests() throws DataAccessException {
+        return userDao.addUser(new RegisterRequest("testUser", "password", "test@email.com"));
+    }
+
+    private GameData addGameForTests() throws DataAccessException {
+        var game = new GameData(1, null, null, "test");
+        //calling update here will actually add the game, and skip normal adding protocols
+        return gameDao.update(game);
+    }
 
 
 
--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -1,9 +1,6 @@
 package service;
 
-import dataaccess.IAuthDAO;
-import dataaccess.IUserDAO;
-import dataaccess.MemoryAuthDAO;
-import dataaccess.MemoryUserDAO;
+import dataaccess.*;
 import dataobjects.UserData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -19,24 +16,25 @@ public class UserServiceTests {
     AuthService authService = new AuthService(authDao);
     UserService userService = new UserService(userDao, authService);
 
-//    @BeforeEach
-//    void clear() throws ResponseException {
-//        userDao.deleteAllUsers();
-//    }
-//
-//    @Test
-//    @DisplayName("Register Valid Test")
-//    public void register(){
-//
-//    }
+    @BeforeEach
+    void clear() throws DataAccessException {
+        userDao.deleteAllUsers();
+    }
 
-//    @Test
-//    @DisplayName("Login Valid Test")
-//    public void login() throws ResponseException {
-//        var user = userDao.addUser(new RegisterRequest("testUser", "password", "test@email.com"));
-//        var login = userService.login(new LoginRequest(user.username(), "password"));
-//
-//        assertTrue(authService.isAuthValid(login.authToken()));
-//    }
+    @Test
+    @DisplayName("Register Valid Test")
+    public void register(){
+
+    }
+
+    @Test
+    @DisplayName("Login Valid Test")
+    public void login() throws DataAccessException {
+        var user = userDao.addUser(new RegisterRequest("testUser", "password", "test@email.com"));
+        var login = userService.login(new LoginRequest(user.username(), "password"));
+        var auth = authService.getAuthByID(login.authToken());
+
+        assertTrue(authService.isAuthValid(auth));
+    }
 
 }
