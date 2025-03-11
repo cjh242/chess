a1f8fba5c07cf48e5e36fd9a26fa4846f923adda
Conway Hogan <conway2019hogan@gmail.com>
2025-03-10 18:28:02 -0600

    database startup file

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -2,31 +2,34 @@ package dataaccess;
 
 import dataobjects.GameData;
 
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
 
 public class GameDAO implements IGameDAO{
-    @Override
+    private final DatabaseStartup dbStarter;
+
+    public GameDAO(DatabaseStartup dbStarter) throws DataAccessException {
+        this.dbStarter = dbStarter;
+        dbStarter.configureDatabase();
+    }
+
     public Collection<GameData> listGames() throws DataAccessException {
         return List.of();
     }
 
-    @Override
     public GameData addGame(String gameName) throws DataAccessException {
         return null;
     }
 
-    @Override
     public void deleteAllGames() throws DataAccessException {
 
     }
 
-    @Override
     public GameData findByID(int gameID) throws DataAccessException {
         return null;
     }
 
-    @Override
     public GameData update(GameData game) {
         return null;
     }
