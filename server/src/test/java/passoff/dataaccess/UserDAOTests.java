31a2c2e44b1f505334b2fd4644f0e587f07b76cf
Conway Hogan <conway2019hogan@gmail.com>
2025-03-12 17:21:38 -0600

    add user test

--- server/src/test/java/passoff/dataaccess/UserDAOTests.java
+++ server/src/test/java/passoff/dataaccess/UserDAOTests.java
@@ -1,4 +1,32 @@
 package passoff.dataaccess;
 
+import dataaccess.DataAccessException;
+import dataaccess.UserDAO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import request.RegisterRequest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UserDAOTests {
+    private final UserDAO userDao = new UserDAO();
+
+    @BeforeEach
+    void clear() throws DataAccessException {
+        userDao.deleteAllUsers();
+    }
+
+    @Test
+    @DisplayName("Add User")
+    public void addUser() throws DataAccessException {
+        var user = userDao.addUser(new RegisterRequest("TEST_USER", "PASSWORD", "TEST@EMAIL.COM"));
+
+        var retrievedUser = userDao.getUserByUsername("TEST_USER");
+
+        assertEquals(user, retrievedUser);
+
+    }
+
+
 }
